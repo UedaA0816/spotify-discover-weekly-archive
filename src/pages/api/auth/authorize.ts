0e4a39b97a3a8108e669af4ed39739c7a3876b44
ueda <>
2021-11-08 14:41:40 +0900

    add tailwind jit & api route

---
+++ src/pages/api/auth/authorize.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { withSessionRoute, withSessionSsr } from "@/lib/withSession";
+
+type SpotifyAuthApiResponse = {
+  access_token: string,
+  token_type: string,
+  scope: string,
+  expires_in: number,
+  refresh_token: string
+}
+
+const authorize = async (req, res) => {
+  const { code, state } = req.query;
+
+  const params = new URLSearchParams();
+  params.append('grant_type', 'authorization_code');
+  params.append('code', code as string);
+  params.append('redirect_uri', process.env.RETURN_TO as string);
+
+  const response = await axios.post<SpotifyAuthApiResponse>(
+      'https://accounts.spotify.com/api/token',
+      params,
+      {
+          headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': `Basic ${Buffer.from(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`, 'utf-8').toString('base64')}`
+          }
+      }
+  );
+
+  req.session.set('user', {
+      accessToken: response.data.access_token,
+  });
+
+  res.status(200).redirect('/');
+};
+
+export default withSessionRoute(authorize);
