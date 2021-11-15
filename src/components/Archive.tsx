c8c0cf2ad9e9af2af94f1e8cf0e914f783d7e207
ueda <>
2021-11-15 18:05:09 +0900

    add フォーム処理

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -1,37 +1,53 @@
 import axios from 'axios';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 import Button from './Button';
 
+type FormData = {
+  playlistName: string;
+  playlistIdOrUrl: string;
+  isUrl:boolean;
+};
+
 function Archive() {
-  const handleArchive = () => {
+  const { register, setValue, handleSubmit, formState: { errors } } = useForm<FormData>({defaultValues:{playlistName:"DiscoverWeekly {date}"}});
+
+  const handleArchive = (data:FormData) => {
+    console.log(data)
+    const {playlistName,playlistIdOrUrl,isUrl} = data
     const param = {
-      playlistName: "DiscoverWeekly {date}",
-      // playlistId:"37i9dQZEVXcSupRFuEvSif",
-      playlistIdUrl: "https://open.spotify.com/playlist/37i9dQZEVXcSupRFuEvSif?si=7d8ea39d1c594e2e",
+      playlistName,
+      ...(!isUrl ? {playlistId:playlistIdOrUrl} : {}),
+      ...(isUrl ? {playlistIdUrl:playlistIdOrUrl} : {})
     }
+    // const param = {
+    //   playlistName: "DiscoverWeekly {date}",
+    //   // playlistId:"37i9dQZEVXcSupRFuEvSif",
+    //   playlistIdUrl: "https://open.spotify.com/playlist/37i9dQZEVXcSupRFuEvSif?si=7d8ea39d1c594e2e",
+    // }
     axios.post("/api/user/discoverweekly/archive", param)
   }
   return (
     <div className="w-[300px] sm:w-[400px]">
-      <form>
+      <form onSubmit={handleSubmit(handleArchive)}>
         <div className="mb-6">
-          <label htmlFor="password" className="text-sm font-medium text-gray-100 block mb-2">Your playlist name</label>
-          <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-spotify focus:border-spotify block w-full p-2.5" placeholder="DiscoverWeekly {date}" />
+          <label htmlFor="playlistName" className="text-sm font-medium text-gray-100 block mb-2">Your playlist name</label>
+          <input type="text" id="playlistName" className="bg-gray-50 border-2 border-gray-300 text-gray-900 sm:text-sm rounded-lg outline-none focus-visible:ring-spotify focus-visible:border-spotify block w-full p-2.5" placeholder="DiscoverWeekly {date}" {...register("playlistName",{required:true})} />
         </div>
         <div className="mb-3">
-          <label htmlFor="email" className="text-sm font-medium text-gray-100 block mb-2">Your playlist ID or URL</label>
-          <input type="text" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-spotify focus:border-spotify block w-full p-2.5" placeholder="0123456789012345678901" />
+          <label htmlFor="playlistId" className="text-sm font-medium text-gray-100 block mb-2">Your playlist ID or URL</label>
+          <input type="text" id="playlistId" className="bg-gray-50 border-2 border-gray-300 text-gray-900 sm:text-sm rounded-lg outline-none focus-visible:ring-spotify focus-visible:border-spotify block w-full p-2.5" placeholder="0123456789012345678901" {...register("playlistIdOrUrl",{required:true})} />
         </div>
         <div className="flex items-start mb-6">
           <div className="flex items-center h-5">
-            <input aria-describedby="it is url" type="checkbox" className="bg-gray-50 border-gray-300 focus:ring-3 focus:ring-spotify h-4 w-4 rounded" />
+            <input id="isUrl" aria-describedby="it is url" type="checkbox" className="bg-gray-50 border-gray-300 focus:ring-3 focus-visible:ring-spotify h-4 w-4 rounded" {...register("isUrl")} />
           </div>
           <div className="text-sm ml-3">
-            <label htmlFor="it is url" className="font-medium text-gray-100">it is URL</label>
+            <label htmlFor="isUrl" className="font-medium text-gray-100">it is URL</label>
           </div>
         </div>
         <div className="text-right">
-          <Button onClick={handleArchive}>Archive</Button>
+          <Button>Archive</Button>
         </div>
       </form>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Button:React.FC<{onClick:()=>void}> = ({children,onClick})=>{
+const Button:React.FC<{onClick?:()=>void}> = ({children,onClick})=>{
   
   return(
     <button className="box-border border-2 border-spotify border-solid bg-spotify rounded-full p-1.5 px-4 font-semibold text-sm" onClick={onClick}>{children}</button>
