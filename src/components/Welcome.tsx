b098f90c9932d367b09331ee6ce9cd2b95933e6d
ueda <>
2021-11-17 16:54:37 +0900

    fix style

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -7,13 +7,17 @@ function Welcome() {
   return (
     <div className="flex flex-col justify-center items-center py-8 min-h-full">
       <div className="flex flex-col justify-center items-center min-h-[50vh]">
-        <p className="text-3xl font-semibold tracking-wide break-words">DiscoverWeeklyのプレイリスト自動保存サービス</p>
-        <p className="text-xl mt-4">まずはログインしましょう</p>
+        <p className="text-3xl font-semibold text-spotify tracking-wide break-words">DiscoverWeeklyのプレイリスト自動保存サービス</p>
+        <p className="text-xl text-gray-0 mt-4">まずはログインしましょう</p>
       </div>
 
-      <div className="mt-20">
-        <OutLink href="https://uedaa0816.github.io/">Contact us</OutLink>
-        <OutLink className="ml-4" href="https://github.com/UedaA0816/spotify-discover-weekly-archive">Welcome PR</OutLink>
+      <div className="mt-20 flex gap-4">
+        <div className="w-28 text-center">
+          <OutLink href="https://uedaa0816.github.io/">Contact us</OutLink>
+        </div>
+        <div className="w-28 text-center">
+          <OutLink href="https://github.com/UedaA0816/spotify-discover-weekly-archive">Welcome PR</OutLink>
+        </div>
       </div>
     </div>
   );
