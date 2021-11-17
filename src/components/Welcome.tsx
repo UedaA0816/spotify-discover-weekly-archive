b9f4d33c6c719621d8ecbe653c5344d416b422c5
ueda <>
2021-11-17 15:47:27 +0900

    add welcome text

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -4,8 +4,13 @@ function Welcome() {
   
   
   return (
-    <div>
-      welcome
+    <div className="flex flex-col justify-center items-center py-8 gap-6 min-h-full">
+      <p className=" text-3xl font-semibold">DiscoverWeeklyのプレイリスト自動保存サービス</p>
+      <p className=" text-base">まずはログインしましょう</p>
+
+      <div className="mt-20">
+        <a className=" border-b border-blue-700 text-blue-700" href="https://uedaa0816.github.io/">Contact us</a>
+      </div>
     </div>
   );
 }
